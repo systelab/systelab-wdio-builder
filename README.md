d1474ec65dbf1c1d0b245489d198b07756fa469a
Joaquim Vila Bou <jvila2@werfen.com>
2022-11-03 00:23:13 +0100

    Initial version adapted for Angular 14.x

---
+++ README.md
@@ -0,0 +1,73 @@
+# systelab-wdio-builder
+
+Angular builder for running e2e tests using webdriver.io runner. Fork from [@migalons/angular-wdio-builder](https://github.com/migalons/angular-wdio-builder) repository (not maintained).
+
+Enables including a WDIO launcher into Angular workspace (angular.json).
+
+## Peer dependencies
+
+- _@wdio/cli_
+
+WDIO client is not installed. You must install this package by yourself, among other related packages (@wdio/async, services, plugins, etc.)
+
+## Requirements
+
+- @angular/cli >= @14.0.0
+
+## Usage
+
+### Install 
+
+npm install systelab-wdio-builder --save-dev
+
+### Angular workspace
+
+For enabling your e2e test using angular builders, you just need to use this builder for running your e2e test:
+
+```
+{
+  "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+  "version": 1,
+  "newProjectRoot": "projects",
+  "projects": {
+    "my-awesome-project": {
+      ...
+      "architect": {
+        ...
+        "e2e": {
+          "builder": "systelab-wdio-builder:test",
+          "options": {
+            "wdioConfig": "e2e/wdio.conf.js",
+            "devServerTarget": "my-awesome-project:serve"
+          },
+          "configurations": {
+            "production": {
+              "wdioConfig": "e2e/wdio-prod.conf.js",
+              "devServerTarget": "my-awesome-project:serve:production"
+            },
+            "dev": {
+              "wdioOptions": {
+                  "port": 4567  // whatever options accepted by wdio cli
+              }
+              "devServerTarget": "my-awesome-project:serve:production",
+              "port": 4201,
+              "host": "0.0.0.0",
+              "disableHostCheck: true"
+            }
+          }
+        }
+      }
+    }
+  ...
+}
+```
+
+This builders, accepts three parameters:
+
+- wdioConfig: URL for WDIO CLI configuration (defaults to "./e2e/wdio.conf.js).
+- wdioOptons: Custom options for overriding provided configuration (wdioConfig). See [wdio cli options](https://webdriver.io/docs/clioptions.html) for more information.
+- devServerTarget: Project target and configuration to be scheduled before running e2e test (i.e. serve app).
+- port: Serve angular application using a custom port
+- host: Host to listen on
+- disableHostCheck: Don't verify connected clients are part of allowed hosts.
+
---
+++ src/command/test.ts
@@ -0,0 +1,79 @@
+import { BuilderContext, BuilderOutput, createBuilder, targetFromTargetString } from '@angular-devkit/architect';
+import { JsonObject } from '@angular-devkit/core';
+
+interface Options extends JsonObject {
+    devServerTarget: string;
+    wdioConfig: string;
+    wdioOptions: {};
+    port: number;
+    host: string;
+    disableHostCheck: boolean;
+}
+
+export const runWdioTest =  async ( options: Options, context: BuilderContext ): Promise<BuilderOutput> => {
+    if(!isWdioInstalled()) {
+        context.logger.error("@wdio/cli not installed. Can not run command. Exiting.");
+        context.reportStatus('Failed');
+        return Promise.resolve({"success": false, "error": "@wdio/cli not installed. Can not run command. Exiting."})
+    }
+
+    let server;
+    if(options.devServerTarget) {
+        const devServerTargetOptions: any = {port: options.port, host: options.host, disableHostCheck: options.disableHostCheck}
+        Object.keys(devServerTargetOptions).forEach(key => devServerTargetOptions[key] === undefined && delete devServerTargetOptions[key])
+
+        server = await context.scheduleTarget(targetFromTargetString(options.devServerTarget), devServerTargetOptions);
+        const result = await server.result;
+        if(!result.success) {
+            return Promise.resolve({"success": false, "error": `${options.devServerTarget} failed. Can not run command. Exiting`})
+        }
+    }
+    const Launcher = require('@wdio/cli').default;
+    const wdio = new Launcher(options.wdioConfig, options.wdioOptions);
+
+    let wdioResult: BuilderOutput | undefined;
+    try {
+        wdioResult = await wdio.run().then(
+            (code: number) => {
+                context.reportStatus(`Done.`);
+                context.logger.info(`Result status code: ${code}`);
+                return { success: code === 0 }
+            },
+            (err: any) => {
+                context.reportStatus(`Failed.`);
+                context.logger.error('Launcher failed to start the test', err.stacktrace)
+                return { 'success': false, error: err.message }
+            }
+        );
+    } catch (err) {
+        context.reportStatus('Exception.');
+        context.logger.error('Exception while running tests', err.message);
+        wdioResult = {
+            error: err.message,
+            success: false
+        };
+    } finally {
+        if (server) {
+            context.logger.info(`Stopping development server...`);
+            await server.stop();
+        }
+    }
+
+    if (!wdioResult) {
+        return { success: false };
+    }
+
+    return wdioResult;
+}
+
+export default createBuilder(runWdioTest);
+
+function isWdioInstalled(): boolean {
+    try {
+        const Launcher = require('@wdio/cli').default;
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
